8fd565dd8337101dbbc50ce6cecc2d9fb99ea7c1
Minh Nguyen <nhminh2301@gmail.com>
2023-12-11 20:11:22 +0700

    + doozy setup && basepopup/view

---
+++ HoppingCats/Assets/Scripts/Core/UI/Base/BaseView.cs
@@ -0,0 +1,28 @@
+﻿using Doozy.Engine.UI;
+using UnityEngine;
+
+namespace vgame
+{
+    [RequireComponent(typeof(UIView))]
+    public abstract class BaseView : MonoBehaviour
+    {
+        #region components
+        private UIView _view;
+        public UIView View { get { if (!_view) _view = GetComponent<UIView>(); return _view; } }
+        #endregion
+
+        #region methods
+        protected virtual void Awake()
+        {
+            View.ShowBehavior.OnStart.Event.AddListener(OnShow);
+            View.HideBehavior.OnFinished.Event.AddListener(OnHide);
+        }
+
+        protected virtual void OnShow()
+        {
+        }
+
+        protected virtual void OnHide() { }
+        #endregion
+    }
+}
