b821fa752f520d1811fb243757d99483f126f061
tuan0170 <106425643+tuan0170@users.noreply.github.com>
2024-02-20 21:50:52 +0700

    * update views

--- HoppingCats/Assets/_Game/Scripts/UI/Views/InGameView.cs
+++ HoppingCats/Assets/_Game/Scripts/UI/Views/InGameView.cs
@@ -1,9 +1,16 @@
 using Doozy.Engine;
+using Doozy.Engine.UI;
 
 public class InGameView : BaseViewExt
 {
+    public UIButton lobbyButton;
     public static void Show()
     {
         GameEventMessage.SendEvent(DoozyEventDefine.kGoToInGame);
-    }    
+    }
+    protected override void Awake()
+    {
+        base.Awake();
+        if(lobbyButton) lobbyButton.OnClick.OnTrigger.Event.AddListener(() => MainView.Show());
+    }
 }
--- HoppingCats/Assets/_Game/Scripts/UI/Views/MainView.cs
+++ HoppingCats/Assets/_Game/Scripts/UI/Views/MainView.cs
@@ -1,9 +1,18 @@
 using Doozy.Engine;
+using Doozy.Engine.UI;
 
 public class MainView : BaseViewExt
 {
+    public UIButton playButton;
+
     public static void Show()
     {
         GameEventMessage.SendEvent(DoozyEventDefine.kGoToMain);
-    }    
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (playButton) playButton.OnClick.OnTrigger.Event.AddListener(() => InGameView.Show());
+    }
 }
