09eaafc5bf4039fae1341063df23e8bf78125184
tuan0170 <106425643+tuan0170@users.noreply.github.com>
2024-02-20 23:51:18 +0700

    Update InGameView.cs

--- HoppingCats/Assets/_Game/Scripts/UI/Views/InGameView.cs
+++ HoppingCats/Assets/_Game/Scripts/UI/Views/InGameView.cs
@@ -11,13 +11,17 @@ public class InGameView : BaseViewExt
     public RectTransform eggGroup;
     public RectTransform healthGroup;
 
+    public static InGameView Ins;
+
     public static void Show()
     {
         GameEventMessage.SendEvent(DoozyEventDefine.kGoToInGame);
     }
+
     protected override void Awake()
     {
         base.Awake();
+        if (!Ins) Ins = this;
         if(lobbyButton) lobbyButton.OnClick.OnTrigger.Event.AddListener(() => MainView.Show());
 
         ShowReadyUI();
