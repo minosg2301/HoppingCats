6872e007e15685a8ebbdb4252bdd5fc0dc9112a8
saladassvn <buiquocdat321@gmail.com>
2024-03-04 18:37:14 +0700

    improve transition fx

--- HoppingCats/Assets/_Game/Scripts/Controller/SwitchViewController.cs
+++ HoppingCats/Assets/_Game/Scripts/Controller/SwitchViewController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using moonNest;
 using System;
 using DG.Tweening;
-
+using UnityEngine.UI;
 public class SwitchViewController : SingletonMono<SwitchViewController>
 {
     public RectTransform container;
     public Animator switchViewAnimator;
     public float showDuration = 1.5f;
     public float hideDuration = 1.1f;
+    public Image bg;
+    public List<Color32> bgColors = new List<Color32>();
 
     public const string kShow = "show";
     public const string kHide = "hide";
@@ -28,7 +30,7 @@ public class SwitchViewController : SingletonMono<SwitchViewController>
     {
         container.gameObject.SetActive(true);
         switchViewAnimator.SetTrigger(kShow);
-
+        GetRandomBGColor();
         DOVirtual.DelayedCall(showDuration, () =>
         {
             onShowDone?.Invoke();
@@ -39,6 +41,17 @@ public class SwitchViewController : SingletonMono<SwitchViewController>
                 OnHideDone();
             });
         });
+    }
+
+    public void GetRandomBGColor()
+    {
+        if (bgColors.Count == 0)
+        {
+            Debug.LogWarning("Dat - No colors available in the list!");
+            return;
+        }
+        Color32 randomColor = bgColors[UnityEngine.Random.Range(0, bgColors.Count)];
+        bg.color = randomColor;
     }
 
     private void OnHideDone()
