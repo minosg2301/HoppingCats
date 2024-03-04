197392689c56635f8b4b55efcb76cb6d46e694d8
tuan0170 <106425643+tuan0170@users.noreply.github.com>
2024-03-04 22:21:03 +0700

    * modify switch view anim

--- HoppingCats/Assets/_Game/Scripts/Controller/SwitchViewController.cs
+++ HoppingCats/Assets/_Game/Scripts/Controller/SwitchViewController.cs
@@ -5,6 +5,9 @@ using moonNest;
 using System;
 using DG.Tweening;
 using UnityEngine.UI;
+using System.Linq;
+using Random = UnityEngine.Random;
+
 public class SwitchViewController : SingletonMono<SwitchViewController>
 {
     public RectTransform container;
@@ -20,23 +23,52 @@ public class SwitchViewController : SingletonMono<SwitchViewController>
     public static Action onHideDone;
     [Header("BackGround")]
     public Image bg;
-    public List<Color32> bgColors = new List<Color32>();
+    public List<Color32> bgColors = new();
     [Header("Pillow")]
-    public List<Sprite> pillowsSprite = new List<Sprite>();
-    public List<Image> pillows = new List<Image>();
+    public List<Sprite> pillowsSprite = new();
+    public List<Color32> pillowColors = new();
+    public List<float> pillowScales = new();
+    public List<Image> pillows = new();
+
+    public UIPillow pillowPrefab;
+    public RectTransform pillowRecttContainer;
+    public List<RectTransform> pillowRectts;
+    private List<UIPillow> pillowPoolOjs = new();
+    public RectTransform logoGameRectt;
+
+    public AnimationCurve titleAnimationCurve;
+
+#if UNITY_EDITOR
+    [Sirenix.OdinInspector.Button]
+    private void GetPos()
+    {
+        pillowRectts.Clear();
+        pillowRectts = pillowRecttContainer.GetComponentsInChildren<RectTransform>().ToList();
+    }
+#endif
+
     protected override void Awake()
     {
         base.Awake();
         container.gameObject.SetActive(false);
+
+        for(int i = 0; i < pillowRectts.Count; i ++)
+        {
+            var newPillow = Instantiate(pillowPrefab, pillowPrefab.rectTransform.parent);
+            Debug.Log($"T--- Pillow {i}");
+            pillowPoolOjs.Add(newPillow);
+        }    
     }
 
     public void Show(Action onDone = null)
     {
         container.gameObject.SetActive(true);
-        switchViewAnimator.SetTrigger(kShow);
+        //switchViewAnimator.SetTrigger(kShow);
         GetRandomBGColor();
         //Dat - May cai sprite pillow ko cung size nen goi nhin bi ky`
         //AssignRandomSpritesToPillows();
+        StartCoroutine(DoMoveIn());
+
         DOVirtual.DelayedCall(showDuration, () =>
         {
             onShowDone?.Invoke();
@@ -66,14 +98,48 @@ public class SwitchViewController : SingletonMono<SwitchViewController>
         onHideDone?.Invoke();
     }
 
-    public void AssignRandomSpritesToPillows()
+    public IEnumerator DoMoveIn()
     {
-        Shuffle(pillowsSprite);
-        for (int i = 0; i < pillows.Count; i++)
+        logoGameRectt.DOScale(0, 0);
+        for (int i = 0; i < pillowPoolOjs.Count; i++)
         {
-            int randomIndex = UnityEngine.Random.Range(0, pillowsSprite.Count);
-            pillows[i].sprite = pillowsSprite[randomIndex];
-        }
+            var currentIndex = i;
+            int spriteIndex = Random.Range(0, pillowsSprite.Count);
+            pillowPoolOjs[currentIndex].icon.sprite = pillowsSprite[spriteIndex];
+
+            int colorIndex = Random.Range(0, pillowColors.Count);
+            pillowPoolOjs[currentIndex].icon.color = pillowColors[colorIndex];
+
+            float scaleValue = Random.Range(.7f, 2f);
+            pillowPoolOjs[currentIndex].icon.rectTransform.DOScale(scaleValue, 0);
+
+            float rotationValue = Random.Range(-25, 25f);
+            pillowPoolOjs[currentIndex].icon.rectTransform.DORotate(new Vector3(0, 0, rotationValue), 0);
+
+            yield return new WaitForSeconds(.06f);
+
+            pillowPoolOjs[currentIndex].rectTransform.SetAsFirstSibling();
+            pillowPoolOjs[currentIndex].gameObject.SetActive(true);
+            var targetPos = new Vector2(pillowRectts[currentIndex].anchoredPosition.x, pillowRectts[currentIndex].anchoredPosition.y - 100);
+            pillowPoolOjs[currentIndex].rectTransform.DOAnchorPos(targetPos, .4f)
+                .OnComplete(()=>
+                {
+                    pillowPoolOjs[currentIndex].rectTransform.DOAnchorPos(pillowRectts[currentIndex].anchoredPosition, .7f);
+                    pillowPoolOjs[currentIndex].rectTransform.DOScale(new Vector2(.9f, 1.2f), .3f)
+                        .OnComplete(() => 
+                        {
+                            pillowPoolOjs[currentIndex].rectTransform.DOScale(new Vector2(1f, 1f), .2f);
+                        });
+
+                    
+                });
+
+            DOVirtual.DelayedCall(1f, () =>
+            {
+                logoGameRectt.DOScale(1, .4f)
+                        .SetEase(titleAnimationCurve);
+            });
+        }    
     }
     private void Shuffle<T>(List<T> list)
     {
