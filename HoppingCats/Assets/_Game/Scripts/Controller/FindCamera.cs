424c42656609ac64fc8ff3722d839086b50d5a53
LongNT <long.nt1608@gmail.com>
2024-03-18 20:49:22 +0700

    * add Sprite

---
+++ HoppingCats/Assets/_Game/Scripts/Controller/FindCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class FindCamera : MonoBehaviour
+{
+    public string cameraName;
+
+    private Canvas _canvas;
+    public Canvas Canvas { get { if (_canvas == null) _canvas = GetComponent<Canvas>(); return _canvas; } }
+
+    void Update()
+    {
+        if(!Canvas.worldCamera)
+        {
+            Canvas.worldCamera = Camera.allCameras.Find(camera => camera.name == cameraName);
+        }
+    }
+}
